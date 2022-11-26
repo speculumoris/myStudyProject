fd9d3f6dcaeacfbe735bceefe26a6b1e7f1dec05
speculumoris <sefa.byndr4@gmail.com>
2022-11-26 16:23:12 +0300

    SchoolProj added

---
+++ src/main/java/SchoolProj/SchoolMain.java
@@ -0,0 +1,18 @@
+package SchoolProj;
+
+import java.util.Scanner;
+
+public class SchoolMain {
+    static Scanner scan = new Scanner(System.in);
+    static School school = new School();
+
+
+    public static void main(String[] args) {
+        school.StudentGiris();
+    }
+
+
+
+
+
+}
